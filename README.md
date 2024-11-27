bb64646db89c59b4ff8f3c13e79355b49307342d
Skeletron01 <benjaminbutler2007@gmail.com>
2024-11-27 21:31:59 +1030

    task complete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # s1dt-deskcheck
+
+original.py is broken code
+fixed.py is new code
+
+dont ask about what this is for :)
---
+++ fixed.py
@@ -0,0 +1,198 @@
+final= 0
+item1= 499
+item2= 399
+item3= 449
+item4= 89
+item5= 89
+item6= 69
+item7= 99
+item8= 99
+item9= 99
+item10= 49
+item11= 99
+item12= 49
+item13= 89
+item14= 79
+item15= 79
+discount1= item1*0.4
+discount2= item2*0.4
+discount3= item3*0.4
+discount4= item4*0.4
+discount5= item5*0.4
+discount6= item6*0.4
+discount7= item7*0.4
+discount8= item8*0.4
+discount9= item9*0.4
+discount10= item10*0.4
+discount11= item11*0.4
+discount12= item12*0.4
+discount13= item13*0.4
+discount14= item14*0.4
+discount15= item15*0.4
+total1= item1 - discount1
+total2= item2 - discount2
+total3= item3 - discount3
+total4= item4 - discount4
+total5= item5 - discount5
+total6= item6 - discount6
+total7= item7 - discount7
+total8= item8 - discount8
+total9= item9 - discount9
+total10= item10 - discount10
+total11= item11 - discount11
+total12= item12 - discount12
+total13= item13 - discount13
+total14= item14 - discount14
+total15= item15 - discount15
+
+print("1. Playstation 4, 2. Xbox One, 3. Nintendo Switch")
+print("4. Dualshock 4 Wireless Controller, 5. Xbox One Wireless Controller")
+print("6. Joycon Controller, 7. FIFA 19, 8. Madden 19, 9. NBA 2K19")
+print("10. Horizon Zero Dawn, 11. Sea of Thieves, 12. No Man’s Sky")
+print("13. Breath of the Wild, 14. Super Mario Odyssey, 15. Mario Tennis Aces")
+print("")
+
+purchase1=int(input("Select item number 1: "))
+if purchase1==1:
+  print(total1)
+  final= final + total1
+elif purchase1==2:
+  print(total2)
+  final= final + total2
+elif purchase1==3:
+  print(total3)
+  final= final + total3
+elif purchase1==4:
+  print(total4)
+  final= final + total4
+elif purchase1==5:
+  print(total5)
+  final= final + total5
+elif purchase1==6:
+  print(total6)
+  final= final + total6
+elif purchase1==7:
+  print(total7)
+  final= final + total7
+elif purchase1==8:
+  print(total8)
+  final= final + total8
+elif purchase1==9:
+  print(total9)
+  final= final + total9
+elif purchase1==10:
+  print(total10)
+  final= final + total10
+elif purchase1==11:
+  print(total11)
+  final= final + total11
+elif purchase1==12:
+  print(total12)
+  final= final + total12
+elif purchase1==13:
+  print(total13)
+  final= final + total13
+elif purchase1==14:
+  print(total14)
+  final= final + total14
+elif purchase1==15:
+  print(total15)
+  final= final + total15
+
+purchase2=int(input("Select item number 2: "))
+if purchase2==1:
+  print(total1)
+  final= final + total1
+elif purchase2==2:
+  print(total2)
+  final= final + total2
+elif purchase2==3:
+  print(total3)
+  final= final + total3
+elif purchase2==4:
+  print(total4)
+  final= final + total4
+elif purchase2==5:
+  print(total5)
+  final= final + total5
+elif purchase2==6:
+  print(total6)
+  final= final + total6
+elif purchase2==7:
+  print(total7)
+  final= final + total7
+elif purchase2==8:
+  print(total8)
+  final= final + total8
+elif purchase2==9:
+  print(total9)
+  final= final + total9
+elif purchase2==10:
+  print(total10)
+  final= final + total10
+elif purchase2==11:
+  print(total11)
+  final= final + total11
+elif purchase2==12:
+  print(total12)
+  final= final + total12
+elif purchase2==13:
+  print(total13)
+  final= final + total13
+elif purchase2==14:
+  print(total14)
+  final= final + total14
+elif purchase2==15:
+  print(total15)
+  final= final + total15
+
+purchase3=int(input("Select item number 3: "))
+if purchase3==1:
+  print(total1)
+  final= final + total1
+elif purchase3==2:
+  print(total2)
+  final= final + total2
+elif purchase3==3:
+  print(total3)
+  final= final + total3
+elif purchase3==4:
+  print(total4)
+  final= final + total4
+elif purchase3==5:
+  print(total5)
+  final= final + total5
+elif purchase3==6:
+  print(total6)
+  final= final + total6
+elif purchase3==7:
+  print(total7)
+  final= final + total7
+elif purchase3==8:
+  print(total8)
+  final= final + total8
+elif purchase3==9:
+  print(total9)
+  final= final + total9
+elif purchase3==10:
+  print(total10)
+  final= final + total10
+elif purchase3==11:
+  print(total11)
+  final= final + total11
+elif purchase3==12:
+  print(total12)
+  final= final + total12
+elif purchase3==13:
+  print(total13)
+  final= final + total13
+elif purchase3==14:
+  print(total14)
+  final= final + total14
+elif purchase3==15:
+  print(total15)
+  final= final + total15
+
+print("")
+print("Total Cost=")
+print(final)
